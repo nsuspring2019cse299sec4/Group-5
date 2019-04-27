7d8b7a0506f1ec4c5bf1da56aca7a0b8f7a7e54b
Moushfiq Khan <mknothotmail@gmail.com>
2019-04-27 14:18:00 +0600

    Form created

---
+++ Project code/findadoc/finddoctor/forms.py
@@ -0,0 +1,11 @@
+from django import forms 
+
+from .models import Appoinment
+
+class AppoinmentForm(forms.ModelForm):
+	class Meta:
+		model = Appoinment
+		fields = [
+			'doctor',
+			'time'
+		]
